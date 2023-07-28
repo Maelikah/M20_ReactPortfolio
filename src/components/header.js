1679b53577a4cf46e818d6e7662b0805ca4cb517
Maelikah <eva.longoria@gmail.com>
2023-07-28 11:42:50 -0600

    Created header.js

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import bgc from "../assets/bgc.jpg"
+function Header(props) {
+    return (
+        <header className="flex-row space-between px-1">
+        <h1>Eva Longoria</h1>
+        <img src={bgc} alt="Cerro de la Silla"></img>
+        {props.children}
+        </header>
+    );
+}
+
+export default Header;
