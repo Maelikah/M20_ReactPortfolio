4e12100a61d016bf42c141a730f05fe70199ece8
Maelikah <eva.longoria@gmail.com>
2023-07-28 11:28:36 -0600

    fixed an error with Navtabs.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
-import Navtabs from './components/Navtabs'
-
+import Navtabs from './components/Navtabs';
 const App = () => <Navtabs/>
 
 export default App;
