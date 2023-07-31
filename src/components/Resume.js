9a82c447f33efa7da71cd1a6bf90c28dd6e84213
Maelikah <eva.longoria@gmail.com>
2023-07-31 09:53:14 -0600

    Created logic for resume page

---
+++ src/components/Resume.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Resume() {
+    return (
+        <section className="my-5">
+        <div className="my-2">
+            <p>
+            📝 <a href="https://www.linkedin.com/in/eva-longoria">Download</a>
+            </p>
+            <h3>Programming Proficiencies</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>API</li>
+                <li>NodeJS</li>
+                <li>MySQL</li>
+                <li>MongoDB</li>
+            </ul>
+        </div>
+        </section>
+    );
+}
+
+export default Resume;
